e3aa0ca484d94263f922c3301b75ec3a803b1e25
Marise <ecopowermcs@gmail.com>
2025-01-06 19:05:02 -0300

    Finalizando a página de regras, 'apenas o estilo'

--- src/components/Buttons/index.jsx
+++ src/components/Buttons/index.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
 import { Button, ButtonContent } from "./styles";
 
 export default function ButtonComponent({ buttonText, handlePress }) {
   return (
     <Button activeOpacity={0.95} onPress={handlePress}>
-      <Text>
-        <ButtonContent>{buttonText}</ButtonContent>
-      </Text>
+      <ButtonContent>{buttonText}</ButtonContent>
     </Button>
   );
 }
